9fbb94b78bb1d213cba10b0267c3dffb468317e5
Siddhant M <meetsiddhant@gmail.com>
2025-02-14 22:28:32 -0500

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -3,25 +3,32 @@ use std::io::{self, Write};
 use std::process;
 
 fn main() {
-    
     let stdin = io::stdin();
     let mut input = String::new();
+
     loop {
         input.clear();
         print!("$ ");
         io::stdout().flush().unwrap();
         stdin.read_line(&mut input).unwrap();
-        if input.trim()=="exit 0" {
-            process::exit(0);
-        }
-        if input.trim().contains("echo") {
-            println!("{}",input.trim().replace("echo ", ""));
-            continue;
-        }
-
-        println!("{}: command not found", input.trim());
         
+        let trimmed_input = input.trim(); // Avoid redundant `trim()` calls
 
+        match trimmed_input {
+            "exit 0" => process::exit(0),
+            input if input.starts_with("echo ") => {
+                println!("{}", input.strip_prefix("echo ").unwrap());
+            }
+            input if input.starts_with("type ") => {
+                let command = input.strip_prefix("type ").unwrap().trim();
+                match command {
+                    "echo" => println!("echo is a shell builtin"),
+                    "exit" => println!("exit is a shell builtin"),
+                    "type" => println!("type is a shell builtin"),
+                    _ => println!("{} not found", command),
+                }
+            }
+            _ => println!("{}: command not found", trimmed_input),
+        }
     }
-
 }
